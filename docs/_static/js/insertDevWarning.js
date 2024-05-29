4711da00e509286bb760cc122dd113a068586ae3
bogdankol <68349689+bogdankol@users.noreply.github.com>
2024-05-29 18:03:46 +0300

    asd

--- docs/_static/js/insertDevWarning.js
+++ docs/_static/js/insertDevWarning.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
+
   const devwarning = $('.devwarning')
 
-  if(devwarning) {
-    const body = $('.wy-body-for-nav')
+  if(devwarning.length) {
     const container = $('.wy-grid-for-nav')
-    // devwarning.after(body).appendTo(container)
     jQuery(jQuery(".devwarning").detach()).appendTo(".wy-grid-for-nav")
     container.addClass('additionalPaddingForMainContent')
   }
+
 })
--- docs/_static/js/sidebar.js
+++ docs/_static/js/sidebar.js
@@ -9,8 +9,6 @@ $(document).ready(function () {
     return removeOverlayAndButtons(screenWidth)
   })
 
-  insertDevWarning()
-
 })
 
 function removeButtons() {
